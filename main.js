94feba633dda9f7a281fd2dee0e18f1ddc1dbf65
Luciano Saavedra <saav0004@algonquinlive.com>
2022-12-14 12:40:50 -0500

    adjust cards

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 let burgerMenu = document.querySelector(".burger__menu");
 let burgerMenuActive = document.querySelector(".nav__div");
-let body = document.querySelector("body");
 
 burgerMenu.addEventListener("click", burgerAppear);
 
 function burgerAppear() {
   burgerMenuActive.classList.toggle("nav__div__active");
   const navMenu = {
+    body: document.querySelector("#body"),
     spanTop: document.querySelector(".burger__span__top"),
     spanCentre: document.querySelector(".burger__span__centre"),
     spanBottom: document.querySelector(".burger__span__bottom"),
