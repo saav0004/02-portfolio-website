375802d7dd2d7a9e020b1833204b5a86a91ba7ed
Luciano Saavedra <saav0004@algonquinlive.com>
2022-12-16 17:01:02 -0500

    complete most of the main part of the site except accessibility

---
+++ js/utils.js
@@ -0,0 +1,17 @@
+function init() {
+  const toTop = document.querySelector(".to-top");
+
+  function handleToTop() {
+    if (window.scrollY > 50) {
+      toTop.classList.add("visible");
+    } else {
+      toTop.classList.remove("visible");
+    }
+  }
+
+  handleToTop();
+
+  toTop && window.addEventListener("scroll", handleToTop);
+}
+
+document.addEventListener("DOMContentLoaded", init);
